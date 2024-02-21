440698c8961d975cb5ebec3584d31a09479d7ff5
Ankit Raj <ankitraja177@gmail.com>
2024-02-21 22:16:50 +0530

    Add dependencies and update layout***
    Added Clerk for auth

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
+
+const page = () => {
+  return (
+    <div>
+      <p>Home</p>
+      <UserButton afterSignOutUrl="/" />
+
+    </div>
+  )
+}
+
+export default page
