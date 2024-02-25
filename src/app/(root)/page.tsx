453917cf2095a798f38ad4d4e18e5bb83daa3e84
Ankit Raj <ankitraja177@gmail.com>
2024-02-25 23:05:49 +0530

    Update CSS styles and configurations, search functionality, and transformation form

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,11 +1,48 @@
+import { Collection } from "@/components/shared/Collection";
+import { navLinks } from "@/constants";
+import { getAllImages } from "@/lib/actions/image.action";
+import Image from "next/image";
+import Link from "next/link";
 
-const page = () => {
+const page = async ({searchParams}:SearchParamProps) => {
+  const page = Number(searchParams?.page) || 1;
+  const searchQuery = (searchParams?.query as string) || '';
+
+  const images = await getAllImages({page, searchQuery})
   return (
-    <div>
-      <p>Home</p>
+    <>
+     <section className="home">
+        <h1 className="home-heading">
+          Restore Your Creative Vision with RestoreMagic 
+        </h1>
 
-    </div>
-  )
-}
+        <ul className="flex-center w-full gap-20">
+          {
+            navLinks.slice(1,5).map((link) => (
+              <Link href={link.route}
+              key={link.route}
+              className="flex-center flex-col gap-2"
+              >
+                <li className="flex-center w-fit rounded-full bg-white p-4">
+                  <Image src={link.icon} alt={link.label} height={24} width={24}  />
+                </li>
+                <p className="p-14-medium text-center text-white">{link.label}</p>
+              </Link>
+            ))
+          }
+        </ul>
+      </section> 
 
-export default page
+      <section className="sm:mt-12">
+          <Collection
+            hasSearch={true}
+            images={images?.data}
+            totalPages={images?.totalPage}
+            page={page}
+          />
+      </section>
+    </>
+  );
+};
+
+export default page;
